578260b52c2c4023d3b9a706a0784dc3710fc76e
Alexander Pisanov <pisanov.alexander@gmail.com>
2016-02-14 20:06:22 +0300

    Update kep_interface.R

--- kep_interface.R
+++ kep_interface.R
@@ -1,4 +1,4 @@
-# Purpose: access interface to Rosstat KEP database by Evgeny Pogrebnyak.
+# Purpose: access interface to Rosstat KEP database by Evgeny Pogrebnyak to retrieve 'zoo' class time-series.
 # Database URL: https://github.com/epogrebnyak/rosstat-kep-data.
 # Code URL: https://github.com/pisanovav/kep_interface.
 # Author(s): Evgeny Pogrebnyak, Alexander Pisanov.
@@ -17,8 +17,8 @@
 library(zoo) # 'zoo' library is required.
 
 get.data.url.kep <- function(frequency)
-#' Returns an URL string. Syntax: get.kep.data.url("a").
-#' frequency (required) - time series frequency. Input: "a", "q", "m".  
+  #' Returns an URL string. Syntax: get.data.url.kep("a").
+  #' frequency (required) - time series frequency. Input: "a", "q", "m".  
 {
   if (!(frequency %in% c("a","q","m"))) stop("Incorrect frequency parameter. Use 'a', 'q' or 'm' in double quotes.")
   
@@ -30,8 +30,8 @@ get.data.url.kep <- function(frequency)
 }
 
 get.data.frame.kep <- function(id,frequency)
-#' Returns an object of class 'data.frame' with data of a selected frequency. Syntax: get.kep.data.frame("a").
-#' frequency (required) - time series frequency.
+  #' Returns an object of class 'data.frame' with data of a selected frequency. Syntax: get.data.frame.kep("a").
+  #' id(required) - time series id (see full list here: https://raw.githubusercontent.com/epogrebnyak/rosstat-kep-data/master/output/varnames.md), frequency(required) - time series frequency.
 {
   data.table <- read.table(get.data.url.kep(frequency),sep = ",",header=TRUE,row.names=1)[id]
   
@@ -39,8 +39,8 @@ get.data.frame.kep <- function(id,frequency)
 }
 
 get.zoo.kep <- function(id,frequency,start.date=NULL,end.date=NULL)
-#' Returns an object of class 'zoo' with a time series of a selected id, frequency and date range. Syntax: get.kep.zoo("CPI_rog","q","1999-01-01","2000-01-01").
-#' id(required) - time series id (see full list here: https://raw.githubusercontent.com/epogrebnyak/rosstat-kep-data/master/output/varnames.md), frequency(required) - time series frequency, start/end.date(optional) - date range.
+  #' Returns an object of class 'zoo' with a time series of a selected id, frequency and date range. Syntax: get.zoo.kep("CPI_rog","q","1999-01-01","2000-01-01").
+  #' id(required) - time series id (see full list here: https://raw.githubusercontent.com/epogrebnyak/rosstat-kep-data/master/output/varnames.md), frequency(required) - time series frequency, start/end.date(optional) - date range.
 {
   data.table <- get.data.frame.kep(id,frequency)
   data.zoo <- zoo(data.table,row.names(data.table))
@@ -50,8 +50,8 @@ get.zoo.kep <- function(id,frequency,start.date=NULL,end.date=NULL)
 }
 
 write.csv.kep <- function(id,frequency,start.date=NULL,end.date=NULL)
-#' Writes a .csv file to current working directory. Syntax: get.kep.zoo("CPI_rog","q","1999-01-01","2000-01-01").
-#' id(required) - time series id (see full list here: https://raw.githubusercontent.com/epogrebnyak/rosstat-kep-data/master/output/varnames.md), frequency(required) - time series frequency, start/end.date(optional) - date range.
+  #' Writes a .csv file to current working directory. Syntax: write.csv.kep("CPI_rog","q","1999-01-01","2000-01-01").
+  #' id(required) - time series id (see full list here: https://raw.githubusercontent.com/epogrebnyak/rosstat-kep-data/master/output/varnames.md), frequency(required) - time series frequency, start/end.date(optional) - date range.
 {
   data.filename <- paste0(id,".csv")
   write.csv(get.zoo.kep(id,frequency,start.date,end.date),file=data.filename,row.names=TRUE,dec=",")
